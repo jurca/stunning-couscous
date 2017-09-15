5023f76a8e2c0fe9ace991062d9743ba4f1717b4
Martin Jurča <mjurca@centrum.cz>
2017-09-15 21:46:48 +0200

    Added prototyping player shit and a collision test

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+	Rigidbody rigidBody;
+
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		rigidBody.AddForce (new Vector3 (0, 0, -1));
+	}
+}
